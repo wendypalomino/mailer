7e15a607f6a48dfc0224249fbf63ac6452c9b0f6
Wendy Palomino <wvanessa.palomino.rivera@gmail.com>
2020-11-28 01:06:34 -0500

    implemented nodemailer-handlebars

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,15 +1,16 @@
 const { Router } = require('express');
 const nodemailer = require('nodemailer');
+const hbs = require('nodemailer-handlebars')
 const router = Router();
 require ('dotenv').config();
 
 router.post('/send-email', async (req, res) => {
     const { name , email } = req.body;
 
-    contentHtml = `
-    <h1>Hola ${name}</h1>
-    <p>Tu correo electrónico ${email} ha sido registrado satisfactoriamente.</p>
-    `
+    // contentHtml = `
+    // <h1>Hola ${name}</h1>
+    // <p>Tu correo electrónico ${email} ha sido registrado satisfactoriamente.</p>
+    // `
     
     let transporter = nodemailer.createTransport({
         host: 'smtp.gmail.com',
@@ -25,11 +26,24 @@ router.post('/send-email', async (req, res) => {
     });
 
 
+    transporter.use("compile",hbs({
+        viewEngine:{
+           partialsDir:"./views/",
+           defaultLayout:""
+       },
+        viewPath:"./views/",
+        extName:".hbs"
+  }));
+
+
     let mailOptions = {
         from: 'testpalominovanessa@gmail.com',
         to: `${email}`,
         subject: 'Testing and Testing',
-        html: contentHtml
+        template: 'welcome',
+        context: {
+            name: `${name}`
+        }
     };
 
     transporter.sendMail(mailOptions, function (err, data) {
